6afea603486b283c93f8d1e469d2ee682e3427e7
Eliad Karni <EliadK995@Gmail.com>
2022-12-25 01:22:07 +0200

    path return test

--- util/util.py
+++ util/util.py
@@ -1,8 +1,13 @@
 import os
+import logging
 
 
 def get_ser_lst(rootpath: str):
-    return [series for series in os.walk(rootpath)]
+    try:
+        return [series for series in os.walk(os.path.join(rootpath, "Series"))][0][1]
+    except Exception as e:
+        logging.exception(e)
+        return []
 
 
 if __name__ == "__main__":
