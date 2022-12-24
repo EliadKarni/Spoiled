9cfc13d924d3ba33664e7a49df9f0dec44166bb7
Eliad Karni <EliadK995@Gmail.com>
2022-12-25 01:11:02 +0200

    path return test

--- util/util.py
+++ util/util.py
@@ -2,7 +2,7 @@ import os
 
 
 def get_ser_lst(rootpath: str):
-    return [series for series in os.walk(rootpath)]#[0][1]
+    return [series for series in os.walk(rootpath)]
 
 
 if __name__ == "__main__":
