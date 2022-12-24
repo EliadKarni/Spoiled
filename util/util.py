9b6628e1705dfc61b672300923ef3ca35ac34459
Eliad Karni <EliadK995@Gmail.com>
2022-12-25 01:34:55 +0200

    doc

--- util/util.py
+++ util/util.py
@@ -2,7 +2,15 @@ import os
 import logging
 
 
-def get_ser_lst(rootpath: str):
+def get_ser_lst(rootpath: str) -> list:
+    """
+    The function enters the Series directory and returns a list of all the directories inside it which means, all
+    the series the application support.
+    If the dir scan fails, the function return an empty list
+    @param rootpath: Because the azure web-app's app.py runs on different path then on the computer, there is
+    a need for the app variable's root path to create the path to the Series path.
+    @return: A list of the series the application support.
+    """
     try:
         return [series for series in os.walk(os.path.join(rootpath, "Series"))][0][1]
     except Exception as e:
