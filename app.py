ead06d8f30bee7d586602f53726d853de3b999a3
Eliad Karni <EliadK995@Gmail.com>
2023-02-18 17:40:14 +0200

    Exceptions return

--- app.py
+++ app.py
@@ -2,18 +2,21 @@ from datetime import datetime
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from util import *
+from Validator import return_errors
 
 app = Flask(__name__)
 CORS(app)
 
 
 @app.route('/')
+@return_errors
 def index():
     print("received series get")
     return get_ser_lst(app.root_path)
 
 
 @app.route('/', methods=['POST'])
+@return_errors
 def check():
     req = request.data
     return req
