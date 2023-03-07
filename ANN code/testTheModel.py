4311f484f6444560f0d508f8f5c628447be56fed
yairdo <73181574+yairdo@users.noreply.github.com>
2023-03-07 15:16:13 +0200

    Add files via upload
    updated files

--- ANN code/testTheModel.py
+++ ANN code/testTheModel.py
@@ -1,24 +1,31 @@
 from keras.models import load_model
 import numpy as np
-from keras.preprocessing import image
-from PIL import Image
 from keras.utils import load_img, img_to_array
+from os import walk
 
+f = []
+for (dirpath, dirnames, filenames) in walk("testData/TGP/"):
+    f.extend(filenames)
+    break
 # load your model
 loaded_model = load_model('my_test_model.h5')
 
 # load an example image
-img_path = 'testData/Avatar/ep1frame174.jpg'
-img = load_img(img_path, target_size=(224, 224))
+class_names = ['Avatar', 'The Good Place']
 
-# preprocess the image to match the input format expected by your model
-img_array = img_to_array(img)
-img_array = np.expand_dims(img_array, axis=0)
-img_array /= 255.0
+# Load and preprocess the image
+for file in f:
+    img_path = "testData/TGP/"
+    image = load_img(img_path+file, target_size=(224, 224))
+    image_array = img_to_array(image)
+    image_array = np.expand_dims(image_array, axis=0)
+    image_array /= 255.
 
-# use the loaded model to make predictions on the image
-predictions = loaded_model.predict(img_array)
+    # Make predictions on the image
+    predictions = loaded_model.predict(image_array)
 
-# print the predictions
-print(predictions)
-
+    # Print the predicted class name and confidence level
+    predicted_class_index = np.argmax(predictions)
+    predicted_class_name = class_names[predicted_class_index]
+    confidence = predictions[0][predicted_class_index] * 100
+    print(f" File Name: {file} Predicted class: {predicted_class_name} ({confidence:.2f}%)")
