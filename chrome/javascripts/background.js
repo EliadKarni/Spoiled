1a58b98a499e0e80ec80ecf17270854df9efb7b3
Michal Bucks <michall1900@gmail.com>
2023-03-12 15:36:27 +0200

    Success with changing images while page is changes but there is a problem with it. Add documentation

--- chrome/javascripts/background.js
+++ chrome/javascripts/background.js
@@ -1,22 +1,39 @@
-const AZURE_URL = "https://spoiledservice.azurewebsites.net"
-const SERIES_PATH = "series"
+//const AZURE_URL = "https://spoiledservice.azurewebsites.net"
+const AZURE_URL = "http://127.0.0.1:16201"
+//const SERIES_PATH = "series"
+const ALARM_NAME = "fetchListAlarm"
+const INSTALL_REASON = "install"
 
+/**
+ * Checks if the chrome extension currently installs
+ * If it is, fetch the series list.
+ */
 chrome.runtime.onInstalled.addListener(details => {
-    if (details.reason === "install")
+    if (details.reason === INSTALL_REASON)
         fetchList()
 })
 
-chrome.alarms.create("fetchListAlarm",{
+/**
+ * Sets an alarm that is fetching the series list every day.
+ */
+chrome.alarms.create(ALARM_NAME,{
     when: Date.now() + 1000,
     periodInMinutes: 60*24
 })
 
+/**
+ * Listen to the alarm event.
+ */
 chrome.alarms.onAlarm.addListener((alarm)=>{
-    if (alarm.name === "fetchListAlarm")
+    if (alarm.name === ALARM_NAME)
         fetchList()
 })
 
-// move this code to utilities
+/**
+ * The function checks if the status code is ok and returns a promise matching the result.
+ * @param response
+ * @returns {Promise<never>|Promise<unknown>}
+ */
 function status(response) {
     if (response.status >= 200 && response.status < 300) {
         return Promise.resolve(response)
@@ -25,13 +42,19 @@ function status(response) {
     }
 }
 
+/**
+ * The function fetches the series list inside the server and saves it in
+ * chrome.storage.local with a key named 'seriesList'.
+ */
 function fetchList(){
-    fetch(`${AZURE_URL}/${SERIES_PATH}`)
+    // fetch(`${AZURE_URL}/${SERIES_PATH}`)
+    fetch(`${AZURE_URL}`)
         .then(status)
         .then(function(response) {
             return response.json();
         })
         .then((list)=>{
+            console.log(list)
                 chrome.storage.local.set({'seriesList': list})
                     .then((item) => console.log(item))
             }
@@ -46,16 +69,20 @@ function fetchList(){
 //     let [tab] = await chrome.tabs.query(queryOptions);
 //     return tab;
 // }
-chrome.tabs.onUpdated.addListener((tabId,changeInfo, tab)=>{
-    if (changeInfo.status === 'complete'){
-        chrome.scripting.executeScript({
-            target: { tabId: tabId},
-            files: ['pageImagesBlocker.js'],
-        }).catch((err)=>{
-            console.log(err)
-        })
-    }
-})
+// chrome.tabs.onUpdated.addListener((tabId,changeInfo, tab)=>{
+//     console.log("update")
+//     if (changeInfo.status === 'complete'){
+//         console.log("complete")
+//         chrome.scripting.executeScript({
+//             target: { tabId: tabId},
+//             files: ['pageImagesBlocker.js']
+//         })
+//         .then(()=>{console.log("success")})
+//         .catch((err)=>{
+//             console.log(err)
+//         })
+//     }
+// })
 // chrome.scripting.executeScript({
 //             target: { tabId: await getCurrentTab().id},
 //             files: ['pageImagesBlocker.js'],
--- chrome/javascripts/pageImagesBlocker.js
+++ chrome/javascripts/pageImagesBlocker.js
@@ -1,45 +1,59 @@
 (function(){
-    const config = {childList: true, subtree:true}
+    console.log("running pageImages")
+    const BLOCK_IMAGE_PATH = chrome.runtime.getURL('images/block.jpg');
+    const config = {childList: true, subtree:true, attributes: true}
+    let observe;
+    const PIC_TAGS = 'source, img, image'
+
+    /**
+     * Change the images for the first time - check if there are elements with tags' names that could hold an image
+     * and send it to server check.
+     */
     const changeImages = ()=>{
-        console.log("============================here=============================")
-        let srcElements = document.querySelectorAll('[src]');
-        // let elementsWithImageOrVideoSrcArr = []
-        // srcElements.forEach((elem)=>{
-        //     if (elem.getAttribute('src').match())
-        // })
+        let srcElements = document.querySelectorAll(PIC_TAGS);
+        console.log(srcElements)
         getServerResults (srcElements)
     }
-
+    /**
+     * This function gets server result for all the sources. (the implementation is temporary)
+     * @param elements
+     */
     const getServerResults = (elements = []) =>{
         // temp implementation, will be change to server's check.
         elements.forEach((elem)=>{
-            elem.setAttribute('src',chrome.runtime.getURL('../images/block.jpg'))
+            if (elem.hasAttribute('src')) {
+                elem.setAttribute('src', BLOCK_IMAGE_PATH)
+            }
+            else if(elem.hasAttribute('srcset')) {
+                elem.setAttribute('srcset', BLOCK_IMAGE_PATH)
+            }
+
         })
     }
 
-    let observer = new MutationObserver((mutations)=>{
+
+    console.log("init")
+
+    //create a mutation observer to handle page changes
+    observe = new MutationObserver((mutations)=>{
         let relevantNewElements = []
         console.log("mutation")
         mutations.forEach((mutation)=>{
-            mutation.addedNodes.forEach((node)=>{
-                if (node.nodeType=== Node.ELEMENT_NODE && node.hasAttribute('src')){
-                    relevantNewElements.push(node)
+            mutation.addedNodes.forEach((node) => {
+                if (node.nodeType === Node.ELEMENT_NODE){
+                    console.log("here")
+                    console.log(node)
+                    let elements = node.querySelectorAll(PIC_TAGS)
+                    console.log(elements)
+                    if (elements.length)
+                        relevantNewElements.push(...node.querySelectorAll(PIC_TAGS))
                 }
             })
         })
         getServerResults(relevantNewElements)
     })
-    if (typeof init === 'undefined'){
-        const init = function(){
-            changeImages()
-            observer.observe(document,config)
-        }
-        init();
-    }
+    changeImages()
+    observe.observe(document,config)
 
-    // document.addEventListener("DOMContentLoaded",(_)=>{
-    //     changeImages()
-    //     observer.observe(document,config)
-    // })
 
 })();
